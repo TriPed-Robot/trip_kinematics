1275947763adb7494b21316417f095c7306e0674
Torben Miller <bearlord14@gmail.com>
2021-03-26 17:36:01 +0100

    Add package structure

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name="Trip ",
+    version="0.9",
+    author="Torben Miller",
+    author_email="torben-miller@web.de",
+    description="...",
+    packages=["trip"]
+)
