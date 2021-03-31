682b13326ba70947f34b677d6236d69626a1efea
Torben Miller <bearlord14@gmail.com>
2021-03-31 20:29:49 +0200

    Fix #3

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,15 @@
 # setup.py
 import setuptools
 
+with open("requirements.txt", "r") as fh:
+    requirements = fh.readlines()
+
 setuptools.setup(
-    name="Trip ",
-    version="0.9",
-    author="Torben Miller",
-    author_email="torben-miller@web.de",
-    description="...",
-    packages=["trip"]
+    name='Trip',
+    version='1.0',
+    author='Torben Miller',
+    license='MIT',
+    description='...',
+    install_requires=[req for req in requirements if req[:2] != "# "],
+    packages=['trip_kinematics']
 )
