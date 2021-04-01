29618cff5e8a874797fe601d57f265e3f3f93acb
Torben Miller <bearlord14@gmail.com>
2021-04-01 21:36:14 +0200

    Fix #1

--- src/trip_kinematics/Robot.py
+++ src/trip_kinematics/Robot.py
@@ -1,12 +1,22 @@
 from trip_kinematics.Joint import Joint
 from typing import Union
 
+   def forward(self):
+        """[summary]
+        """
+        pass
+
+    def inverse(self):
+        """[summary]
+        """
+        pass
+
 
 def dummy_function():
     pass
 
 
-class OpenChain:
+class Robot:
     """[summary]
     """
 
@@ -19,16 +29,6 @@ class OpenChain:
         """
         self.__joints = []
         self.__mapping = {'test': dummy_function}
-        pass
-
-    def forward(self):
-        """[summary]
-        """
-        pass
-
-    def inverse(self):
-        """[summary]
-        """
         pass
 
     def get_actuated_joints(self):
