5a78f5b8102f7dfab90e10a9d4dcf9e47310a5c6
Torben Miller <bearlord14@gmail.com>
2021-04-01 21:43:22 +0200

    Split joint into group and joint

--- src/trip_kinematics/Joint.py
+++ src/trip_kinematics/Joint.py
@@ -1,49 +1,7 @@
 
 from trip_kinematics.HomogenTransformationMartix import Homogenous_transformation_matrix
-from typing import Union
-from typing import Dict, List
-
-
-class State:
-    """Representation of the state of a group of kinematic joints
-
-    Attributes:
-        __constants (Dict[str, float]): All the constants values inside the group of joints
-        variables (Dict[str, float]): All the variables (DOF) of a group of joints
-    """
-
-    def __init__(self, values: Dict[str, float], adjustable: List[str]) -> None:
-        """Sorts values into either constants or variables
-
-        Args:
-            values (Dict[str, float]): Dictionary of all values both constants and variables
-            adjustable (List[str]): List of all the variables inside values
-
-        Raises:
-            ValueError: An error is raised if there is a key in adjustable that doesn't match any from values
-        """
-        if not set(adjustable) <= set(values.keys()):
-            raise ValueError(
-                "Key(s) from adjustable not present inside values")
-
-        self.__constants = {}
-        self.variables = {}
-
-        adjust = '#'.join(adjustable)
-
-        for key in values.keys():
-            if adjust.find(key) != -1:
-                self.variables.setdefault(key, values.get(key))
-            else:
-                self.__constants.setdefault(key, values.get(key))
-
-    def get_constants(self) -> Dict[str, float]:
-        """Getter for constants
-
-        Returns:
-            Dict[str, float]: All the constants inside a dict
-        """
-        return self.__constants
+from typing import Dict, List, Union
+from trip_kinematics.Group import State
 
 
 class JointState(State):
--- src/trip_kinematics/examples/triped.py
+++ src/trip_kinematics/examples/triped.py
@@ -1,11 +1,11 @@
 from trip_kinematics.Joint import Joint, State, JointState
 from math import radians
-from trip_kinematics.OpenChain import OpenChain
+from trip_kinematics.Robot import Robot
 from trip_kinematics.HomogenTransformationMartix import Homogenous_transformation_matrix
 
 if __name__ == '__main__':
 
-    # openchain
+    # robot
     gimbal_joint = JointState(
         {'alpha': 0, 'beta': 0, 'gamma': 0, 'x': 0.265, 'z': 0.014}, ['alpha', 'beta', 'gamma'])
 
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     def fun():
         pass
 
-    open_chain = OpenChain([A_CCS_P, A_P_LL_Joint, A_LL_Joint_FCS], [fun])
+    robot = Robot([A_CCS_P, A_P_LL_Joint, A_LL_Joint_FCS], [fun])
 
     gimbal_joint.variables['alpha'] = 1000
     A_CCS_P.change_value(gimbal_joint)
