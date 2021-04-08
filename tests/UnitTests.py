a756dac3fb93e94a3249e23051c92e2f7d9b7d28
Torben Miller <bearlord14@gmail.com>
2021-04-08 15:59:27 +0200

    Update JointState

--- tests/UnitTests.py
+++ tests/UnitTests.py
@@ -1,27 +1,16 @@
 import unittest
-from trip_kinematics.Joint import State
+from trip_kinematics.Joint import JointState
 
 
 class TestStates(unittest.TestCase):
 
-    def test_state_constructor_input(self):
-        # One variables and one constant
-        val_input_1 = {"t1": 1, "t2": 3}
-        adjustable_input_1 = ["t1"]
+    def test_joint_state_Constructor(self):
 
-        state_1 = State(values=val_input_1, adjustable=adjustable_input_1)
+        self.assertRaises(ValueError, JointState, {"b": 0})
 
-        self.assertDictEqual(state_1.variables, {"t1": 1}, "")
-        self.assertDictEqual(state_1.get_constants(), {"t2": 3}, "")
+        joint_state = JointState({'q0': 1})
 
-        # No variables and no constants
-        val_input_2 = {}
-        adjustable_input_2 = []
-
-        state_2 = State(values=val_input_2, adjustable=adjustable_input_2)
-
-        self.assertDictEqual(state_2.get_constants(), {}, "")
-        self.assertDictEqual(state_2.variables, {}, "")
+        self.assertEqual(joint_state.get_state(), {'q0': 1})
 
 
 if __name__ == '__main__':
