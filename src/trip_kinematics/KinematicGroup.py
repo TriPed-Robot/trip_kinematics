9458b3785076823b6d0f07559487e236241ae78f
Torben Miller <bearlord14@gmail.com>
2021-04-08 18:16:00 +0200

    Remove redundant ParameterSpecification

--- src/trip_kinematics/KinematicGroup.py
+++ src/trip_kinematics/KinematicGroup.py
@@ -1,40 +1,6 @@
 from typing import Dict, List, Callable
 
 
-class ParameterSpecification:
-    """Used for initilizing a KinematicObject
-
-    Attributes:
-        __constants (Dict[str, float]): All the constants values inside the group of joints
-        variables (Dict[str, float]): All the variables (DOF) of a group of joints
-    """
-
-    def __init__(self, values: Dict[str, float], adjustable: List[str]) -> None:
-        """Sorts values into either constants or variables
-
-        Args:
-            values (Dict[str, float]): Dictionary of all values both constants and variables
-            adjustable (List[str]): List of all the variables inside values
-
-        Raises:
-            ValueError: An error is raised if there is a key in adjustable that doesn't match any from values
-        """
-        if not set(adjustable) <= set(values.keys()):
-            raise ValueError(
-                "Key(s) from adjustable not present inside values")
-
-        self.constants = {}
-        self.variables = {}
-
-        adjust = '#'.join(adjustable)
-
-        for key in values.keys():
-            if adjust.find(key) != -1:
-                self.variables.setdefault(key, values.get(key))
-            else:
-                self.constants.setdefault(key, values.get(key))
-
-
 class Group:
     """[summary]
     """
