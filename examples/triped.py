ac38bfe10454523885a55aeda77867a141f99a8c
Torben Miller <bearlord14@gmail.com>
2021-04-19 23:04:03 +0200

    Restructure example f_mapping

--- examples/triped.py
+++ examples/triped.py
@@ -9,92 +9,92 @@ from math import radians
 from tf.transformations import quaternion_from_euler
 
 
-def c(rx, ry, rz, opti):
-    A_CSS_P = Homogenous_transformation_matrix(
-        tx=0.265, ty=0, tz=0.014, rx=rx, ry=ry, rz=rz, conv='xyz')
-
-    T_P_SPH1_2 = np.array([-0.015, -0.029, 0.0965]) * -1
-    T_P_SPH2_2 = np.array([-0.015, 0.029, 0.0965]) * -1
-    x0, y0, z0 = T_P_SPH1_2
-    x1, y1, z1 = T_P_SPH2_2
-
-    A_P_SPH1_2 = Homogenous_transformation_matrix(
-        tx=x0, ty=y0, tz=z0, conv='xyz')
-    A_P_SPH2_2 = Homogenous_transformation_matrix(
-        tx=x1, ty=y1, tz=z1, conv='xyz')
-
-    A_c1 = A_CSS_P * A_P_SPH1_2
-    A_c2 = A_CSS_P * A_P_SPH2_2
-
-    c1 = A_c1.get_translation()
-    c2 = A_c2.get_translation()
-
-    c1_mx = opti.variable(3, 1)
-    c1_mx[0, 0] = c1[0]
-    c1_mx[1, 0] = c1[1]
-    c1_mx[2, 0] = c1[2]
-
-    c2_mx = opti.variable(3, 1)
-    c2_mx[0, 0] = c2[0]
-    c2_mx[1, 0] = c2[1]
-    c2_mx[2, 0] = c2[2]
-
-    c1 = c1_mx
-    c2 = c2_mx
-    return c1, c2
-
-
-def p1(theta, opti):
-    A_CCS_lsm_tran = Homogenous_transformation_matrix(
-        tx=0.139807669447128, ty=0.0549998406976098, tz=-0.051)
-
-    A_CCS_lsm_rot = Homogenous_transformation_matrix(
-        rz=radians(-338.5255), conv='xyz')
-
-    A_CCS_lsm = A_CCS_lsm_tran * A_CCS_lsm_rot
-
-    A_MCS1_JOINT = Homogenous_transformation_matrix(rz=theta, conv='xyz')
-
-    A_CSS_MCS1 = A_CCS_lsm * A_MCS1_JOINT
-
-    A_MCS1_SP11 = Homogenous_transformation_matrix(tx=0.085, ty=0, tz=-0.0245)
-
-    A_CCS_SP11 = A_CSS_MCS1 * A_MCS1_SP11
-
-    p1 = A_CCS_SP11.get_translation()
-    p1_mx = opti.variable(3, 1)
-    p1_mx[0, 0] = p1[0]
-    p1_mx[1, 0] = p1[1]
-    p1_mx[2, 0] = p1[2]
-    return p1_mx
-
-
-def p2(theta, opti):
-    A_CCS_rsm_tran = Homogenous_transformation_matrix(
-        tx=0.139807669447128, ty=-0.0549998406976098, tz=-0.051)
-
-    A_CCS_rsm_rot = Homogenous_transformation_matrix(
-        rz=radians(-21.4745), conv='xyz')
-
-    A_CCS_rsm = A_CCS_rsm_tran*A_CCS_rsm_rot
-
-    A_MCS2_JOINT = Homogenous_transformation_matrix(rz=theta, conv='xyz')
-
-    A_CSS_MCS2 = A_CCS_rsm * A_MCS2_JOINT
-
-    A_MCS2_SP21 = Homogenous_transformation_matrix(tx=0.085, ty=0, tz=-0.0245)
-
-    A_CSS_SP21 = A_CSS_MCS2 * A_MCS2_SP21
-
-    p2 = A_CSS_SP21.get_translation()
-    p2_mx = opti.variable(3, 1)
-    p2_mx[0, 0] = p2[0]
-    p2_mx[1, 0] = p2[1]
-    p2_mx[2, 0] = p2[2]
-    return p2_mx
-
-
 def mapping_f(state: Dict[str, float]):
+
+    def c(rx, ry, rz, opti):
+        A_CSS_P = Homogenous_transformation_matrix(
+            tx=0.265, ty=0, tz=0.014, rx=rx, ry=ry, rz=rz, conv='xyz')
+
+        T_P_SPH1_2 = np.array([-0.015, -0.029, 0.0965]) * -1
+        T_P_SPH2_2 = np.array([-0.015, 0.029, 0.0965]) * -1
+        x0, y0, z0 = T_P_SPH1_2
+        x1, y1, z1 = T_P_SPH2_2
+
+        A_P_SPH1_2 = Homogenous_transformation_matrix(
+            tx=x0, ty=y0, tz=z0, conv='xyz')
+        A_P_SPH2_2 = Homogenous_transformation_matrix(
+            tx=x1, ty=y1, tz=z1, conv='xyz')
+
+        A_c1 = A_CSS_P * A_P_SPH1_2
+        A_c2 = A_CSS_P * A_P_SPH2_2
+
+        c1 = A_c1.get_translation()
+        c2 = A_c2.get_translation()
+
+        c1_mx = opti.variable(3, 1)
+        c1_mx[0, 0] = c1[0]
+        c1_mx[1, 0] = c1[1]
+        c1_mx[2, 0] = c1[2]
+
+        c2_mx = opti.variable(3, 1)
+        c2_mx[0, 0] = c2[0]
+        c2_mx[1, 0] = c2[1]
+        c2_mx[2, 0] = c2[2]
+
+        c1 = c1_mx
+        c2 = c2_mx
+        return c1, c2
+
+    def p1(theta, opti):
+        A_CCS_lsm_tran = Homogenous_transformation_matrix(
+            tx=0.139807669447128, ty=0.0549998406976098, tz=-0.051)
+
+        A_CCS_lsm_rot = Homogenous_transformation_matrix(
+            rz=radians(-338.5255), conv='xyz')
+
+        A_CCS_lsm = A_CCS_lsm_tran * A_CCS_lsm_rot
+
+        A_MCS1_JOINT = Homogenous_transformation_matrix(rz=theta, conv='xyz')
+
+        A_CSS_MCS1 = A_CCS_lsm * A_MCS1_JOINT
+
+        A_MCS1_SP11 = Homogenous_transformation_matrix(
+            tx=0.085, ty=0, tz=-0.0245)
+
+        A_CCS_SP11 = A_CSS_MCS1 * A_MCS1_SP11
+
+        p1 = A_CCS_SP11.get_translation()
+        p1_mx = opti.variable(3, 1)
+        p1_mx[0, 0] = p1[0]
+        p1_mx[1, 0] = p1[1]
+        p1_mx[2, 0] = p1[2]
+        return p1_mx
+
+    def p2(theta, opti):
+        A_CCS_rsm_tran = Homogenous_transformation_matrix(
+            tx=0.139807669447128, ty=-0.0549998406976098, tz=-0.051)
+
+        A_CCS_rsm_rot = Homogenous_transformation_matrix(
+            rz=radians(-21.4745), conv='xyz')
+
+        A_CCS_rsm = A_CCS_rsm_tran*A_CCS_rsm_rot
+
+        A_MCS2_JOINT = Homogenous_transformation_matrix(rz=theta, conv='xyz')
+
+        A_CSS_MCS2 = A_CCS_rsm * A_MCS2_JOINT
+
+        A_MCS2_SP21 = Homogenous_transformation_matrix(
+            tx=0.085, ty=0, tz=-0.0245)
+
+        A_CSS_SP21 = A_CSS_MCS2 * A_MCS2_SP21
+
+        p2 = A_CSS_SP21.get_translation()
+        p2_mx = opti.variable(3, 1)
+        p2_mx[0, 0] = p2[0]
+        p2_mx[1, 0] = p2[1]
+        p2_mx[2, 0] = p2[2]
+        return p2_mx
+
     theta_right = state['t1']
     theta_left = state['t2']
     opti = Opti()
