ae2aac04130f924a2176ceed8f554a23b1590fbb
Torben Miller <bearlord14@gmail.com>
2021-08-01 22:31:43 +0200

    Remove old tests and add tests for mappings

--- tests/UnitTests.py
+++ tests/UnitTests.py
@@ -1,33 +1,75 @@
 import unittest
-from trip_kinematics.KinematicChainPart import KinematicChainPart
-from trip_kinematics.KinematicObject import KinematicObject
+from trip_kinematics.Robot import Robot, forward_kinematics, inverse_kinematics
+from trip_kinematics.KinematicGroup import KinematicGroup, Transformation
+from math import radians, degrees
+from copy import deepcopy
+
+# Simple scara robot
+
+arm_1 = Transformation(name="arm_1", values={'tx': 1})
+joint = Transformation(name="joint", values={'rz': 0}, state_variables=['rz'])
+arm_2 = Transformation(name="arm_2", values={'tx': 1})
+
+
+def f_mapping(state):
+    rot = state[0]['z']
+    return [{}, {'rz': rot}, {}]
+
+
+def g_mapping(state):
+    rot = state[1]['rz']
+    return[{'z': rot}]
 
 
 class TestStates(unittest.TestCase):
 
-    def test_KinematicChainPart(self):
-        parent = KinematicChainPart("test")
-        kinematic_chain = KinematicChainPart("test", parent)
+    def test_set_actuated_state(self):
+        scara_arm = KinematicGroup(name="scara", virtual_transformations=[
+            arm_1, joint, arm_2], actuated_state=[{'z': 0}], f_mapping=f_mapping, g_mapping=g_mapping)
 
-        self.assertEqual(kinematic_chain.get_name(), "test")
+        scara = Robot([scara_arm])
 
-        self.assertEqual(kinematic_chain.get_parent(), parent)
+        inital_position = forward_kinematics(scara)
+        scara_arm.set_actuated_state([{'z': radians(90)}])
+        end_position = forward_kinematics(scara)
 
-        self.assertEqual(parent.get_child(), kinematic_chain)
+        self.assertEqual(str(list(inital_position)), str([2.0, 0.0, 0.0]))
+        self.assertEqual(str(list(end_position)), str([1.0, 1.0, 0.0]))
 
-        self.assertRaises(NotImplementedError, kinematic_chain.get_state)
+    def test_trivial_f_mapping(self):
+        scara_arm = KinematicGroup(name="scara", virtual_transformations=[
+            arm_1, joint, arm_2])
+        scara = Robot([scara_arm])
+        inital_position = forward_kinematics(scara)
+        scara_arm.set_actuated_state([{'joint_rz': radians(90)}])
+        end_position = forward_kinematics(scara)
 
-        self.assertRaises(NotImplementedError, kinematic_chain.set_state, {})
+        self.assertEqual(str(list(inital_position)), str([2.0, 0.0, 0.0]))
+        self.assertEqual(str(list(end_position)), str([1.0, 1.0, 0.0]))
 
-        self.assertRaises(NotImplementedError,
-                          kinematic_chain.get_transformation)
+    def test_set_virtual_state(self):
+        scara_arm = KinematicGroup(name="scara", virtual_transformations=[
+            arm_1, joint, arm_2], actuated_state=[{'z': 0}], f_mapping=f_mapping, g_mapping=g_mapping)
 
-    def test_KinematicObject(self):
-        self.assertRaises(ValueError, KinematicObject,
-                          name='test', values={'h': 1})
+        scara = Robot([scara_arm])
+        inital_angle = inverse_kinematics(scara, [2.0, 0.0, 0.0])
+        scara_arm.set_virtual_state([{}, {'rz': radians(90)}, {}])
+        end_angle = inverse_kinematics(scara, [1.0, 1.0, 0.0])
 
-        self.assertRaises(ValueError, KinematicObject, name='test', values={
-                          'x': 0, 'y': 0}, stateVariables=['q1'])
+        self.assertEqual(str(inital_angle), str([[[{'z': 0.0}]]]))
+        self.assertEqual(str(end_angle), str([[[{'z': radians(90)}]]]))
+
+    def test_trivial_g_mapping(self):
+        scara_arm = KinematicGroup(name="scara", virtual_transformations=[
+            arm_1, joint, arm_2])
+        scara = Robot([scara_arm])
+        inital_angle = inverse_kinematics(scara, [2.0, 0.0, 0.0])
+        scara_arm.set_virtual_state(
+            [{}, {'rz': radians(90)}, {'rz': radians(90)}, {}])
+        end_angle = inverse_kinematics(scara, [1.0, 1.0, 0.0])
+
+        self.assertEqual(str(inital_angle), str([[[{'joint_rz': 0.0}]]]))
+        self.assertEqual(str(end_angle), str([[[{'joint_rz': radians(90)}]]]))
 
 
 if __name__ == '__main__':
