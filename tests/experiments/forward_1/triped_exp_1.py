03dd63b896b2432b9da0ef1fc7fdfb8a42dfc401
Torben Miller <bearlord14@gmail.com>
2021-08-04 09:02:37 +0200

    Add experiment forward 1

---
+++ tests/experiments/forward_1/triped_exp_1.py
@@ -0,0 +1,35 @@
+from triped import triped_leg, gimbal_joint, extend_motor
+from trip_kinematics.Robot import forward_kinematics
+import csv
+
+if __name__ == '__main__':
+
+    filename_input = 'tests/experiments/forward_1/input_data/left_extend_right.csv'
+    filename_output = 'tests/experiments/forward_1/output_data/forward_foot_coordinates.csv'
+
+    input_t1 = []
+    input_t2 = []
+    input_e = []
+
+    with open(filename_input, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            input_t1.append(float(row[0]))
+            input_t2.append(float(row[2]))
+            input_e.append(float(row[1]))
+
+    output_rows = []
+    for i in range(len(input_t1)):
+        gimbal_joint.set_actuated_state([
+            {'t1': input_t1[i], 't2': input_t2[i]}])
+        print(gimbal_joint.get_virtual_state())
+        extend_motor.set_actuated_state(
+            [{'LL_revolute_joint_ry': input_e[i]}])
+        row = forward_kinematics(triped_leg)
+
+        output_rows.append(row)
+
+    with open(filename_output, 'w') as f:
+        writer = csv.writer(f)
+        for row in output_rows:
+            writer.writerow(row)
