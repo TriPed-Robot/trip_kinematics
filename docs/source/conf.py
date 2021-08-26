7165595ae9a0dca50716adabb44620e62fa7773d
Jan Baumgärnter <22688144+liquidcronos@users.noreply.github.com>
2021-08-26 15:07:14 +0200

    added trip logo to doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -102,7 +102,7 @@ html_theme = 'sphinx_rtd_theme'
 # documentation.
 #
 html_theme_options = {'style_nav_header_background':'#2f4858','logo_only':True}
-html_logo = "trip_logo_dark.svg"
+html_logo = "trip_logo_dark.png"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
