77b68ac5b353843e924a765a07d12481ff783640
Mihai Verzan <rtyh12@gmail.com>
2022-01-13 17:55:18 +0200

    Do a bit of refactoring + pep8 stuff

--- tests/test_URDFParser.py
+++ tests/test_URDFParser.py
@@ -4,34 +4,29 @@ import xml.etree.ElementTree as ET
 
 import kinpy as kp
 import numpy as np
-# from trip_kinematics.URDFParser import align_vectors
+from trip_kinematics import Utility
 import trip_kinematics.URDFParser
 import trip_kinematics.Robot
 
 urdf_examples_dir = os.path.join('tests', 'urdf_examples')
 
 
-def state_to_kinpy(state):
+def to_kp(state):
     return {
         "_".join(joint_name.split("_")[:-2]): value
         for joint_name, value in state.items()
     }
 
 
-def state_to_trip(state):
+def to_trip(state):
     return state
 
 
-def get_state(robot):
+def initialize_state(robot):
     return {
         joint_name: 0
         for joint_name in robot.get_actuated_state()
     }
-
-
-def create_trip_robot(path):
-    chain_trip = trip_kinematics.URDFParser.from_urdf(path)
-    return trip_kinematics.Robot(chain_trip)
 
 
 def create_kinpy_chain(path):
@@ -49,46 +44,39 @@ def get_joint_tree_dict(path):
     return trip_kinematics.URDFParser._build_joint_tree_dict(joints)
 
 
-def run_test(path):
-    # setup TriP robot using the URDF parser
-    # this needs a try catch
+def compare_urdf_trip_vs_kinpy(path):
     path = os.path.join(urdf_examples_dir, path)
-    robot = create_trip_robot(path)
-    state = get_state(robot)
 
-    # setup kinpy chain
-    # this needs a try catch
-    chain_kinpy = create_kinpy_chain(path)
-    chain_kinpy.forward_kinematics(state_to_kinpy(state))
+    # Setup TriP robot using the URDF parser
+    try:
+        robot = trip_kinematics.Robot(trip_kinematics.URDFParser.from_urdf(path))
+        state_init = initialize_state(robot)
+        robot.set_actuated_state(to_trip(state_init))
+    except ValueError as err:
+        raise ValueError(f'File {path} contains unupported joint type ({err})') from err
 
-    # this needs a try catch?
-    robot.set_actuated_state(state_to_trip(state))
+    # Setup kinpy chain
+    try:
+        chain_kinpy = create_kinpy_chain(path)
+        chain_kinpy.forward_kinematics(to_kp(state_init))
+    except KeyError as err:
+        raise ValueError(f'File {path} contains unupported joint type ({err})') from err
 
-    joint_tree_dict = get_joint_tree_dict(path)
+    for joint, joint_dict in get_joint_tree_dict(path).items():
+        # Calculate homogenous transformation matrix of kinpy forward kinematics
+        transf_kp = chain_kinpy.forward_kinematics(to_kp(state_init))[joint_dict['child_link']]
 
-    for joint, joint_dict in joint_tree_dict.items():
-        # calculate homogenous matrix of kinpy forward kinematic for every joint
-        transf_kp = chain_kinpy.forward_kinematics(state_to_kinpy(state))[joint_dict['child_link']]
-
-        transf_kp_hom_pos = \
-            trip_kinematics.Utility.hom_translation_matrix(*transf_kp.pos) \
-            .astype('float64')
-        transf_kp_hom_rot = \
-            trip_kinematics.Utility.hom_rotation(
-                trip_kinematics.Utility.quat_rotation_matrix(*transf_kp.rot).astype('float64')
-            ) \
-            .astype('float64')
+        kp_rot_matrix = Utility.quat_rotation_matrix(*transf_kp.rot).astype('float64')
+        transf_kp_hom_rot = Utility.hom_rotation(kp_rot_matrix).astype('float64')
+        transf_kp_hom_pos = Utility.hom_translation_matrix(*transf_kp.pos).astype('float64')
 
         transf_kp_hom = transf_kp_hom_pos @ transf_kp_hom_rot
 
-        # calculate homogenous matrix of TriP forward kinematic for every joint
-        transf_trip_hom = \
-            trip_kinematics.forward_kinematics(robot, joint).astype('float64')
+        # Calculate homogenous matrix of TriP forward kinematic for every joint
+        transf_trip_hom = trip_kinematics.forward_kinematics(robot, joint).astype('float64')
 
-        # compare the two matrices and ensure they are close within reason
-        if not np.allclose(transf_kp_hom, transf_trip_hom):
-            return False
-    return True
+        # Compare the two matrices and ensure they are close within reason
+        assert np.allclose(transf_kp_hom, transf_trip_hom)
 
 
 class TestStates(unittest.TestCase):
@@ -96,33 +84,33 @@ class TestStates(unittest.TestCase):
     # tests, skipping for now
 
     def test_all_urdf_files(self):
-        # test of single fixed joint
-        assert run_test("one_fixed_joint.urdf")
+        # Test of single fixed joint
+        compare_urdf_trip_vs_kinpy("one_fixed_joint.urdf")
 
-        # test of single continuous joint
-        assert run_test("one_continuous_joint.urdf")
+        # Test of single continuous joint
+        compare_urdf_trip_vs_kinpy("one_continuous_joint.urdf")
 
-        # test of single revolute joint
-        assert run_test("one_revolute_joint.urdf")
+        # Test of single revolute joint
+        compare_urdf_trip_vs_kinpy("one_revolute_joint.urdf")
 
-        # test of single prismatic joint
-        assert run_test("one_prismatic_joint.urdf")
+        # Test of single prismatic joint
+        compare_urdf_trip_vs_kinpy("one_prismatic_joint.urdf")
 
-        # test of fixed and revolute joints combined
-        assert run_test("fixed_to_revolute_joint.urdf")
+        # Test of fixed and revolute joints combined
+        compare_urdf_trip_vs_kinpy("fixed_to_revolute_joint.urdf")
 
-        # test of prismatic and continuous joints combined
-        assert run_test("prismatic_to_continuous_joint.urdf")
+        # Test of prismatic and continuous joints combined
+        compare_urdf_trip_vs_kinpy("prismatic_to_continuous_joint.urdf")
 
-        # test of large tree
-        assert run_test("large_tree_test.urdf")
+        # Test of large tree
+        compare_urdf_trip_vs_kinpy("large_tree_test.urdf")
 
-        # test of multiple connections to "ground"
-        assert run_test("test.urdf")
+        # Test of multiple connections to "ground"
+        compare_urdf_trip_vs_kinpy("test.urdf")
 
     def test_align_vectors(self):
         test_cases = [
-            ([1, 2, 3], [4, 5, 6]),     # random angle
+            ([1, 2, 3], [4, 5, 6]),     # Random angle
             ([1, 0, 0], [0, 1, 0]),     # 90 degrees
             ([1, 0, 0], [-1, 0, 0]),    # -180 degrees
             ([0, 0, 1], [0, 0, -1]),    # -180 degrees
